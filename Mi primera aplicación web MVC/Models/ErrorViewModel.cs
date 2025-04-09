c3d2a2ed42b67abb982c14d29745da0c10a8933b
laboratorio <laboratorio@UPOAULA10711>
2025-04-09 08:30:21 -0500

    Agregar archivos de proyecto.

---
+++ Mi primera aplicación web MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mi_primera_aplicación_web_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

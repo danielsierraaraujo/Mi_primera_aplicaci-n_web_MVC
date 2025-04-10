824154b2150fd3aa9446563e7d8fda4bd6193694
Sierra10 <daniel.sierra@udla.edu.ec>
2025-04-10 07:45:26 -0500

    Commit 1 creacion de modelos

---
+++ Mi primera aplicación web MVC/Models/Equipo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mi_primera_aplicación_web_MVC.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Display(Name = "Nombre Equipo")]
+        [Required]
+        public string Nombre { get; set; }
+        [Range(0, 20)]
+        public int PartidosJugados { get; set; }
+        [Range(0, 20)]
+        public int PartidosGanados { get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos { get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados { get; set; }
+        public int Puntos {
+            get
+            {
+                int puntos = PartidosGanados * 3 + PartidosEmpatados * 1;
+                return puntos;
+            } 
+        }
+    }
+}
